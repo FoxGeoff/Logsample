abdd48318645d5c96a2a30a6fbc5d52e523e8992
Geoff Fox <fox.geoffrey@gmail.com>
2022-03-11 14:34:06 -0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogTestComponent } from './log-test/log-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogTestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/log-test/log-test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-test',
+  templateUrl: './log-test.component.html',
+  styleUrls: ['./log-test.component.css']
+})
+export class LogTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  testLog() {}
+
+}
